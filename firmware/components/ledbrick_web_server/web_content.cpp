15107b44a0a3e45d96379841935b95ea4e65b543
Yann Ramin <github@theatr.us>
2025-09-11 13:36:08 -0700

    Fix moon calcs

--- firmware/components/ledbrick_web_server/web_content.cpp
+++ firmware/components/ledbrick_web_server/web_content.cpp
@@ -1,5 +1,5 @@
 // Auto-generated web content - DO NOT EDIT
-// Generated from source files with timestamp: 2025-09-11T04:41:46.929Z
+// Generated from source files with timestamp: 2025-09-11T20:14:05.196Z
 // Total size: 126956 bytes compressed
 
 #include "web_content.h"
