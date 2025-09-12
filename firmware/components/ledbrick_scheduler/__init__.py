4e02ebd0d613e204645717b5a51932efc8d3e3d1
Yann Ramin <github@theatr.us>
2025-09-12 12:55:34 -0700

    Always update (#9)
    * Always update
    
    * 5s
    
    * Fix PWM
    
    * Less often scheduler
    
    * Tweaking PWM

--- firmware/components/ledbrick_scheduler/__init__.py
+++ firmware/components/ledbrick_scheduler/__init__.py
@@ -37,7 +37,7 @@ CONF_CURRENT_VALUES = "current_values"
 CONF_PRESET_NAME = "preset_name"
 CONF_ENABLED = "enabled"
 
-DEFAULT_UPDATE_INTERVAL = "1s"
+DEFAULT_UPDATE_INTERVAL = "5s"
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(LEDBrickScheduler),
